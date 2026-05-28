0c574b8a3086070fe3b2b4aad16c0a8cf204df23
lmtuong225949 <luongmanhtuongln@gmail.com>
2026-05-28 15:23:35 +0700

    feat: implement multi-role selection portal, logout redirect and configure Vite MPA build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        admin: resolve(__dirname, 'admin_app.html'),
+        doctor: resolve(__dirname, 'doctor_app.html'),
+        expert: resolve(__dirname, 'expert_app.html'),
+        patient: resolve(__dirname, 'patient_app.html'),
+      },
+    },
+  },
 })
